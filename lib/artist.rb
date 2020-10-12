4565a7f802b98ec0bf88f4d7a271c88c68dda4ec
Chris Houghton <chrishoughton2434@gmail.com>
2020-10-12 00:47:09 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+
+class Artist 
+  
+  attr_accessor :artist 
+  
+  def initialize(artist)
+    @artist = artist
+  end
+end
---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+
+class Author 
+  
+  attr_accessor :author
+  
+  def initialize(author)
+    @author = author
+  end
+end
